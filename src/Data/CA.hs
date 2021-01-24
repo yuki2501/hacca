f06aeb381e87a18233f15517adbcdf4958bc2763
yuki2501 <yuki2501@protonmail.com>
2021-01-25 00:51:33 +0900

    change function type.

--- src/Data/CA.hs
+++ src/Data/CA.hs
@@ -2,11 +2,11 @@ module Data.CA(CA(getUpdateLine,updateAndAddLine,reformField,applyRule,neighbors
 import qualified Data.Array.Repa as Rp
 class CA a where
   getUpdateLine :: a -> Int-- 更新するべき列を取得(DIM1ならラスト1列、DIM2なら全部)
-  updateAndAddLine :: a -> Int ->  Rp.Array Rp.U Rp.DIM2 Bool-- (applyRuleして更新した列を全体に加える,DIM2ならidでいいですが)
-  reformField :: a -> Rp.Array Rp.U Rp.DIM2 Bool -> a -- 変形する必要があるならここで
+  updateAndAddLine :: a -> Int ->  a-- (applyRuleして更新した列を全体に加える,DIM2ならidでいいですが)
+  reformField :: a -> a -- 変形する必要があるならここで
   applyRule :: a -> [Bool] -> Bool -- ルールの適用
   neighbors :: a -> Int -> [Int] -- 近傍の取得
   fieldContentUpdate :: a -> a -- フィールドを更新し、更新されたフィールドを持つaを返す
-  fieldContentUpdate ca = incrementGeneration $ reformField ca (updateAndAddLine ca (getUpdateLine ca))
+  fieldContentUpdate  ca = incrementGeneration $ reformField  $ updateAndAddLine  ca $ getUpdateLine ca 
 
   incrementGeneration :: a -> a
