e4726400d1ad7c408ac30980a032a3483d9c1446
yuki2501 <yuki2501@protonmail.com>
2021-01-26 07:01:13 +0900

    add comment and very small fix.

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,9 @@
 module Main where
-import Data.CA
 import Data.DIM1CA
 import Gloss.Render
 import Graphics.Gloss.Interface.Pure.Game
 import qualified Data.Array.Repa as Rp
-import Control.Monad.ST
+-- example of DIM1 Cellular Automaton (rule110)
 main :: IO()
 main = do
   let field = initfield
@@ -18,11 +17,8 @@ tickHandler :: Float -> Dim1CA -> Dim1CA
 tickHandler _ ca = fieldContentUpdate ca
 
 initfield :: Rp.Array Rp.U Rp.DIM2 Bool
-initfield = let falsecell =  Rp.computeUnboxedS $ Rp.fromFunction (Rp.Z Rp.:.1 Rp.:.1) mkFalse
-             in Rp.computeUnboxedS $ falsecell Rp.++ Rp.computeUnboxedS (Rp.fromFunction (Rp.Z Rp.:.1 Rp.:.1) mkTrue) Rp.++ falsecell
-            where
-              mkTrue _ = True
-              mkFalse _ = False
+initfield = let falsecell =  Rp.computeUnboxedS $ Rp.fromFunction (Rp.Z Rp.:.1 Rp.:.1) (\ _ -> False)
+             in Rp.computeUnboxedS $ falsecell Rp.++ Rp.computeUnboxedS (Rp.fromFunction (Rp.Z Rp.:.1 Rp.:.1) (\ _ -> True)) Rp.++ falsecell
 
 renderField :: Dim1CA -> Picture
 renderField ca = renderArray white 1 (field ca)
